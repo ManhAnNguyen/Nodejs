be85e07d3912eb951f4526de9939d7387efc537b
ManhAnNguyen <thungantn1999@gmail.com>
2022-05-04 16:10:16 +0700

    add express

--- server.js
+++ server.js
@@ -1,109 +1,49 @@
-const logEvents = require("./logEvent");
-const EventEmitter = require("events");
-const http = require("http");
+const express = require("express");
+const app = express();
 const path = require("path");
-const fs = require("fs");
-const fsPromises = require("fs").promises;
-class Emitter extends EventEmitter {}
-//init object
-const myEmitter = new Emitter();
-myEmitter.on("log", (msg, fileName) => logEvents(msg, fileName));
-//add listender for log event
 const PORT = process.env.PORT || 3500;
 
-const serveFile = async (filePath, contentType, response) => {
-  try {
-    const rawData = await fsPromises.readFile(
-      filePath,
+app.get("^/$|/index(.html)?", (req, res) => {
+  // res.sendFile("./views/index.html", { root: __dirname });
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+app.get("/new-page(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "new-page.html"));
+});
+app.get("/old-page(.html)?", (req, res) => {
+  res.redirect(301, "/new-page.html"); //302 by default
+});
 
-      !contentType.includes("image") ? "utf8" : ""
-    );
-    const data =
-      contentType === "application/json" ? JSON.parse(rawData) : rawData;
-    response.writeHead(filePath.includes("404.html") ? 404 : 200, {
-      "Content-Type": contentType,
-    });
-    response.end(
-      contentType === "application/json" ? JSON.stringify(data) : data
-    );
-  } catch (err) {
-    console.log(err);
-    myEmitter.emit("log", `${err.name}:${err.err.message}`, "errLog.txt");
-
-    response.statusCode = 500;
-    response.end();
+//Route handlers
+app.get(
+  "/hello(.html)?",
+  (req, res, next) => {
+    console.log("attemped to load hello.html");
+    next();
+  },
+  (req, res) => {
+    res.send("Hello world");
   }
+);
+
+// chaining route handlers
+const one = (req, res, next) => {
+  console.log("one");
+  next();
 };
 
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
-  myEmitter.emit("log", `${req.url}\t${req.method}`, "reqLog.txt");
-  const extension = path.extname(req.url);
-  console.log("extension", extension);
-  let contentType;
-  switch (extension) {
-    case ".css":
-      contentType = "text/css";
-      break;
-    case ".js":
-      contentType = "text/javascript";
-      break;
-    case ".json":
-      contentType = "application/json";
-      break;
-    case ".jpeg":
-      contentType = "image/jpeg";
-      break;
-    case ".png":
-      contentType = "image/png";
-      break;
-    case ".text":
-      contentType = "text/plain";
-      break;
-    default:
-      contentType = "text/html";
-  }
+const two = (req, res, next) => {
+  console.log("two");
+  next();
+};
 
-  let filePath =
-    contentType === "text/html" && req.url === "/"
-      ? path.join(__dirname, "views", "index.html")
-      : contentType === "text/html" && req.url.slice(-1) === "/"
-      ? path.join(__dirname, "views", req.url, "index.html")
-      : contentType === "text/html"
-      ? path.join(__dirname, "views", req.url)
-      : path.join(__dirname, req.url);
-
-  // make the html extension not require in browser
-  if (!extension && req.url.slice(-1) !== "/") {
-    filePath += ".html";
-  }
-
-  const fileExists = fs.existsSync(filePath);
-
-  if (fileExists) {
-    //serve the files
-    serveFile(filePath, contentType, res);
-  } else {
-    //404
-
-    //301 redirect
-    switch (path.parse(filePath).base) {
-      case "old-page.html":
-        res.writeHead(301, { Location: "/new-page.html" });
-        res.end();
-        break;
-      case "www-page.html":
-        res.writeHead(301, { Location: "/" });
-        res.end();
-        break;
-      default:
-        //serve 404 response
-        serveFile(path.join(__dirname, "views", "404.html"), "text/html", res);
-    }
-  }
+const three = (req, res) => {
+  console.log("three");
+  res.send("Finished!");
+};
+app.get("/chain(.html)?", [one, two, three]);
+app.get("/*", (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html")); //200 by default
 });
-// myEmitter.on("log", (msg) => {
-//   logEvents(msg);
-// });
 
-server.listen(PORT, () => console.log(`Server running in port : ${PORT}`));
+app.listen(PORT, () => console.log(`Server running in port : ${PORT}`));
